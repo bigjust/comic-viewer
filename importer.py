b8ba1f9bd17ea1c51d7e660342f9a556033bf899
Justin Caratzas <justin.caratzas@coxinc.com>
2012-02-19 19:15:26 -0500

    remove user from import info, fix path to something sane, and document usage

--- importer.py
+++ importer.py
@@ -9,7 +9,6 @@ from db import Comic
 import settings
 
 class Importer(object):
-
     def locate(self, pattern, root=os.curdir):
         for path, dirs, files in os.walk(os.path.abspath(root)):
             for filename in fnmatch.filter(files, pattern):
@@ -20,41 +19,39 @@ class Importer(object):
             name = os.path.splitext(os.path.basename(comic))[0]
             self.addComic(title=name,
                           path=comic,
-                          user='jcaratzas',
                           collection=collection)
 
-    def cleanUserDir(self, user):
+    def cleanTmpDir(self, user):
         import shutil
 
-        shutil.rmtree('static/users/'+user)
-        os.mkdir('static/users/'+user)
+        shutil.rmtree('tmp')
+        os.mkdir('tmp')
 
     def addComic(self, title, path, user, collection=""):
         new_comic = Comic(title = title,
                           path = path,
-                          user = user,
                           collection = collection)
         new_comic.save()
 
-        self.cleanUserDir(user)
+        self.cleanTmpDir(user)
 
         splitfile = os.path.splitext(new_comic.path)
         if splitfile[1] == '.cbr':
-            cmd = r'unrar e "%s" static/users/%s' % (new_comic.path, user)
+            cmd = r'unrar e "%s" tmp/%s' % (new_comic.path, user)
             os.system(cmd)
-            files = os.listdir('static/users/'+user)
+            files = os.listdir('tmp')
             files.sort()
         else:
             try:
                 z = zipfile.ZipFile(file(new_comic.path))
-                z.extractall('static/users/'+user)
+                z.extractall('tmp')
                 files=z.namelist()
             except zipfile.BadZipfile:
                 return
 
         cleanfiles = []
         for filename in files:
-            if os.path.isfile('static/users/' + user + '/' + filename):
+            if os.path.isfile('tmp/' + filename):
                 cleanfiles.append(filename)
 
         new_comic.page_count = len(cleanfiles)
@@ -66,9 +63,13 @@ class Importer(object):
 
             k = Key(bucket)
             k.key = filename
-            k.set_contents_from_filename('static/users/'+user+'/'+pic, policy='public-read')
+            k.set_contents_from_filename('tmp/'+pic, policy='public-read')
 
 def main():
+    """ Usage:
+    python importer.py /path/to/comics [Collection Name]
+    """
+
     import sys
     if (len(sys.argv) > 1):
         path = sys.argv[1]
